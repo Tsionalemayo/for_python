c8638598d0d6e70eecab9cd70268c07ce1fe8dd8
ציונה אלמיו <tsionalemayo73@gmail.com>
2022-03-09 20:51:15 +0200

    for exercises

---
+++ for exercises.py
@@ -0,0 +1,36 @@
+#2
+# x=1
+# y=20
+#
+# for a in range(x,y):
+#     if a%2==1 :
+#         print(a)
+
+#3.2
+# x=int(input("enter number"))
+# y=int(input("enter number"))
+#
+# for a in range(x,y):
+#     if(a%2==1):
+#         print(a)
+
+#7
+# x=10
+# for a in range(1,11):
+#     print(a*1,a*2,a*3,a*4,a*5,a*6,a*7,a*8,a*9,a*10)
+
+#13
+# x=100
+# y=5001
+# z=4
+# for a in range(x,y,z):
+#     print(a)
+
+
+ #14
+# x=5
+# y=2500
+#
+# for a in range(x,y):
+#     if a%6==0:
+#         print(a)
